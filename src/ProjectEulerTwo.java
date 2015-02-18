49ecde7f79bed6426f042884d225ba2192b5b1fb
wnalle <nalle@hawaii.edu>
2015-02-17 17:54:15 -1000

    Refactored for testing and CheckStyle.

--- src/ProjectEulerTwo.java
+++ src/ProjectEulerTwo.java
@@ -1,14 +1,36 @@
 /**
+ * Calculates the sum of all even number in a Fibonacci sequence from 2 up to 4000000.
+ * Refactored for testing purposes.
+ *
  * Created by wgnalle on 2/1/2015.
  */
 public class ProjectEulerTwo {
+  /**
+   * Calculates the sum of all even number in a Fibonacci sequence from 2 up to 4000000 and prints out the result.
+   * @param args Ignored
+   */
   public static void main(String[] args) {
-    int sum = 0;
-    int curFib = 1;
-    int prevFib = 0;
+    long sum = calculateFib(2, 4000000);
 
-    for (int i = 2; i < 4000000; ) {
+    System.out.println("The sum of the even-valued terms is " + sum);
+  }
 
+  /**
+   * Refactored code to calculate the sum of even numbers in a
+   * fibonacci sequence given start and stop values.
+   *
+   * @param start Value to start counting from.
+   * @param stop Value to stop counting.
+   * @return The total sum of all even numbers in a Fibonacci sequence between <code>start</code> and <code>stop</code>
+   */
+  public static long calculateFib(long start, long stop) {
+
+    long sum = 0;
+    long curFib = 1;
+    long prevFib = 0;
+    long i = start;
+
+    while (i < stop) {
       System.out.printf("i = %d\n", i);
 
       if (i % 2 == 0) {
@@ -20,6 +42,6 @@ public class ProjectEulerTwo {
 
     }
 
-    System.out.println("The sum of the even-valued terms is " + sum);
+    return sum;
   }
 }
